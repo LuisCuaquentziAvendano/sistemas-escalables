23d0a34c4a5da7fc825452021703a1cbe4dba765
Luis Antonio Cuaquentzi Avendaño <luis.cuaquentzi@gmail.com>
2025-12-04 16:14:15 -0600

    fix: avg cpu utilization modified to a lower value

--- ec2.tf
+++ ec2.tf
@@ -48,7 +48,7 @@ resource "aws_autoscaling_policy" "cpu_policy" {
     predefined_metric_specification {
       predefined_metric_type = "ASGAverageCPUUtilization"
     }
-    target_value = 50.0
+    target_value = 30.0
   }
 }
 
